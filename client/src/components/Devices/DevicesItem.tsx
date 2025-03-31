86dd1ff28cd3ff81cea5f15acdfef1705ea40abd
Theodore Uzun <uzuntudor@yahoo.com>
2025-03-31 10:57:30 +0300

    updating

--- client/src/components/Devices/DevicesItem.tsx
+++ client/src/components/Devices/DevicesItem.tsx
@@ -1,88 +1,94 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { useLocale, useTranslations } from 'next-intl';
-import { Button } from '@chakra-ui/react';
+import { checkImageUrl } from '@/helpers';
 import { useUser } from '@/store/store';
 import { DevicesProps } from '@/store/store.interface';
+import { Button } from '@chakra-ui/react';
+import { useLocale, useTranslations } from 'next-intl';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 import { Icon } from '../Icon/Icon';
-import { checkImageUrl } from '@/helpers';
-
 import './Devices.scss';
 
 interface DeviceItemProps {
-  device: DevicesProps;
+	device: DevicesProps;
 }
 
 export const DevicesItem = ({ device }: DeviceItemProps) => {
-  const locale = useLocale();
-  const t = useTranslations('Categories');
-  const [activeFavoritesIds, addToFavorites, loading, error] = useUser((state) => [
-    state.activeFavoritesIds,
-    state.addToFavorites,
-    state.loading,
-    state.error
-  ]);
-  const activeAddToFavorites = activeFavoritesIds?.find((favoriteId) => favoriteId === device.id);
+	const locale = useLocale();
+	const t = useTranslations('Categories');
+	const [imgSrc, setImgSrc] = useState(checkImageUrl(device?.imageUrl));
+	const [activeFavoritesIds, addToFavorites, loading, error] = useUser((state) => [
+		state.activeFavoritesIds,
+		state.addToFavorites,
+		state.loading,
+		state.error,
+	]);
+	const activeAddToFavorites = activeFavoritesIds?.find((favoriteId) => favoriteId === device.id);
 
-  return (
-    <div className="device--item">
-      {device && (
-        <>
-          <Link href={`/${locale}/device/${device.link}`}>
-            <Image
-              src={checkImageUrl(device?.imageUrl)}
-              alt={device?.name}
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: '100%', height: 'auto' }}
-              priority
-            />
-          </Link>
-          <Link href={`/${locale}/device/${device.link}`}>
-            <div className="device--title">{device.name}</div>
-          </Link>
-          <div className="device--price-wrapper options-devices-card-wrapper">
-            <div className="device--price">
-              {device.credit && (
-                <span>
-                  {device.credit} {t('credit')}
-                </span>
-              )}
+	const handleImageError = () => {
+		setImgSrc('/images/placeholder.webp');
+	};
 
-              <h5>
-                {device.price} {t('lei')}
-              </h5>
+	return (
+		<div className='device--item'>
+			{device && (
+				<>
+					<Link href={`/${locale}/device/${device.link}`}>
+						<Image
+							src={imgSrc}
+							alt={device?.name}
+							width={0}
+							height={0}
+							sizes='100vw'
+							style={{ width: '100%', height: 'auto' }}
+							priority
+							onError={handleImageError}
+						/>
+					</Link>
+					<Link href={`/${locale}/device/${device.link}`}>
+						<div className='device--title'>{device.name}</div>
+					</Link>
+					<div className='device--price-wrapper options-devices-card-wrapper'>
+						<div className='device--price'>
+							{device.credit && (
+								<span>
+									{device.credit} {t('credit')}
+								</span>
+							)}
 
-              {device.cashback && (
-                <span>
-                  Cashback {device.cashback} {t('lei')}
-                </span>
-              )}
-            </div>
-            <div className="options-devices">
-              <div className="compare-devices">
-                <Button
-                // onClick={() => addToCompare(product)}
-                // className={userCompareFind ? 'added-to-compare' : ''}
-                >
-                  <Icon type="compare" />
-                </Button>
-                <div className="options-devices-info compare-info">{t('compare')}</div>
-              </div>
-              <div className="add-to-favorites">
-                <Button
-                  onClick={() => addToFavorites(device.id)}
-                  id={activeAddToFavorites ? 'added-to-favorites' : ''}
-                >
-                  <Icon type="heart" />
-                </Button>
-                <div className="options-devices-info favorites-info">{t('favorites')}</div>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+							<h5>
+								{device.price} {t('lei')}
+							</h5>
+
+							{device.cashback && (
+								<span>
+									Cashback {device.cashback} {t('lei')}
+								</span>
+							)}
+						</div>
+						<div className='options-devices'>
+							<div className='compare-devices'>
+								<Button
+								// onClick={() => addToCompare(product)}
+								// className={userCompareFind ? 'added-to-compare' : ''}
+								>
+									<Icon type='compare' />
+								</Button>
+								<div className='options-devices-info compare-info'>{t('compare')}</div>
+							</div>
+							<div className='add-to-favorites'>
+								<Button
+									onClick={() => addToFavorites(device.id)}
+									id={activeAddToFavorites ? 'added-to-favorites' : ''}
+								>
+									<Icon type='heart' />
+								</Button>
+								<div className='options-devices-info favorites-info'>{t('favorites')}</div>
+							</div>
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
--- client/src/helpers/checkImageUrl.ts
+++ client/src/helpers/checkImageUrl.ts
@@ -1,9 +1,9 @@
 import { apiBaseUrl } from './baseUrl';
 
 export const checkImageUrl = (url: string) => {
-  if (url?.includes('https')) {
-    return url;
-  } else {
-    return `${apiBaseUrl}${url}`;
-  }
+	if (url?.includes('https')) {
+		return url;
+	} else {
+		return `${apiBaseUrl}${url}`;
+	}
 };
